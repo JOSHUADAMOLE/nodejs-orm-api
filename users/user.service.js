7c5556d0107fc787b37bc9d2178d78cd8a7cd31a
JOSHUADAMOLE <joshuadamole123@gmail.com>
2024-03-19 16:17:12 +0800

    updated

---
+++ users/user.service.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function getAll() {
+    return await db.User.findAll();
+}
+
+async function getById(id){
+    return await getUSer(id);
+}
+
+async function create(params) {
+    //validate
+    if (await db.User.findOne({ where: { email: params.email}})) {
+        throw 'Email "' + params.email +'" is already registered';
+    }
+ 
+    const user = new db.User(params);
+
+    //hash password
+    user.passwordHash = await bcrypt.hash(params.password, 10);
+
+    //save user
+    await user.save();
+}
+
+async function update(id, params) {
+    const user = await getUser(id);
+
+    //validate
+    const usernameChanged = params.username && user.username !== params.username;
+    if (userChanged && await db.User.findOne({ where: { username: params.username }})) {
+        throw 'Username "' + params.username +'" is already taken';
+    }
+
+    //hash password if it was entered
+    if (params.password) {
+        params.passwordHash = await bcrypt.hash(params.password, 10);
+    }
+
+    Object.assign(user, params);
+    await user.save();
+}
+
+async function _delete(id) {
+    const user = await getUSer(id);
+    await user.destroy();
+}
+
+async function getUSer(id) {
+    const user = await db.User.findByPk(id);
+    if (!user) throw 'User not found';
+    return user;
+}
